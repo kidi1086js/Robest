b975a163ab5f5714f8e27252fd0c44065abb35b0
Jitulex <41115806+kidi1086js@users.noreply.github.com>
2022-01-15 23:19:52 +0530

    Added Servo Support to the library
    Now can utilize 3 servos by using only one line of code.

--- Robest.h
+++ Robest.h
@@ -1,12 +1,15 @@
 #ifndef Robest_H
 #define Robest_H
 #include "Arduino.h"
-
+#include <Servo.h>
 class Robest {
 public:
   Robest(int r1, int r2, int l1, int l2, int spd);
-
+  Servo s1;
+  Servo s2;
+  Servo s3;
   bool pass;
+  bool S1,S2,S3;
   float BatteryRead, BatteryValue;
 
   int RedVal;
@@ -20,7 +23,9 @@ public:
 
   char data;
 
-  
+  void enableServo1(bool on1);
+  void enableServo2(bool on2);
+  void enableServo3(bool on3);
   void testRun();
   void autoRunBT();
   void init(int baud);
@@ -31,6 +36,10 @@ public:
   void left();
   void setSpeed(int SPD);
   void checkBat(int OPT);
+  int getSliderVal1();
+  int getSliderVal2();
+  int getSliderVal3();
+
 };
 
 
