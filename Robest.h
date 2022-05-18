79c10b66b649453c895e5b88d98831d4eeef6c6c
Jitulex <41115806+kidi1086js@users.noreply.github.com>
2022-05-18 15:36:12 +0530

    Update Robest.h

--- Robest.h
+++ Robest.h
@@ -3,15 +3,20 @@
 #include "Arduino.h"
 #include <Servo.h>
 class Robest {
-public:
+ public:
   Robest(int r1, int r2, int l1, int l2, int spd);
   Servo s1;
   Servo s2;
   Servo s3;
   bool pass;
   bool S1,S2,S3;
-  float BatteryRead, BatteryValue;
-
+  bool OASEN, OASisinitiated = 0;
+  bool obstacle = 0;
+  bool fwd;
+  bool lightattach, hornattach, pwm0attach, pwm1attach, pwm2attach = 0;
+  float BatteryRead, BatteryValue, distance, duration;
+  float BattPercentage;
+  int x;
   int RedVal;
   int GreenVal;
   int BlueVal;
@@ -20,15 +25,34 @@ public:
   int _r1;
   int _r2;
   int _spd;
+  int minOASdistance;
+  int echo;
+  int trig;
 
   char data;
 
+  void IOinit(int pin, char BTVAL, char btval);
+  void PWM0init(bool PWM0ATTACH);
+  void PWM1init(bool PWM1ATTACH);
+  void PWM2init(bool PWM2ATTACH);
+  void lightinit(bool LIGHTATTACH);
+  void horninit(bool HORNATTACH);
+  void PWM0();
+  void PWM1();
+  void PWM2();
+  void light();
+  void horn();
+  void OASinit(int ECHO, int TRIG);
+  void OAS();
   void enableServo1(bool on1);
   void enableServo2(bool on2);
   void enableServo3(bool on3);
+  void lineFollower();
+  void Read();
   void testRun();
+  void run();
   void autoRunBT();
-  void init(int baud);
+  void init(unsigned long baud);
   void forward();
   void stop();
   void backward();
@@ -39,6 +63,8 @@ public:
   int getSliderVal1();
   int getSliderVal2();
   int getSliderVal3();
+  char getdata();
+  float BatteryLevel();
 
 };
 
